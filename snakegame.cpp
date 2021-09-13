ddb7117f345ac47aa55950fc570e95a05a7b07ad
Marcos André <marcos32andre@gmail.com>
2021-09-13 00:09:33 -0300

    arrumado algumas coisas do codigo e dado inicio a função que ira ser o menu do jogo

--- snakegame.cpp
+++ snakegame.cpp
@@ -271,11 +271,9 @@ void logo()
 
 void apaga()
 {
-  int i = MAXCIMA - 1;
-  int j = MAXLADO - 1;
-  for(i = MAXCIMA - 1; i >= meioY - 2 ; i--)
+  for(int i = MAXCIMA - 1; i >= meioY - 2 ; i--)
   {
-    for(j = MAXLADO - 1 ;  j >= meioX - 6; j--)
+    for(int j = MAXLADO - 1 ;  j >= meioX - 6; j--)
     {
       if(i == meioY - 2 && j >= meioX - 4 && j <= meioX + 5)
       {
@@ -310,6 +308,11 @@ void apaga()
   }
 
   }
+}
+
+void menu()
+{
+  
 }
 
 int main()
