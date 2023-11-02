ffcb2fa99c44e1bc6329915c362a03c27e2699e7
Marcos Andre <marcos32andre@gmail.com>
2023-11-01 22:32:04 -0300

    refact: Improved some implemetation and update the initial menu

--- GeneralConfiguration.cpp
+++ GeneralConfiguration.cpp
@@ -35,56 +35,26 @@ void iniciaCampo()
 void logo()
 {
   int k = -1;
-  string linha1 = "SNAKE GAME BY MARCOS ANDRE AND JOAO TADINI";
+  string linha1 = "SNAKE GAME BY MARCOS ANDRE";
 
-  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++)
-  {
-    for(int j = meioX - 10;  j <=  MAXLADO - 1 ; j++)
-    {
-      if (i == meioY- 2 && j <= meioX +5 && j >= meioX - 4)
-      {
-
+  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++){
+    for(int j = meioX - 10;  j <=  MAXLADO - 1 ; j++){
+      if (i == meioY- 2 && j <= meioX +5 && j >= meioX - 4) {
         k++;
         campo[i][j] = linha1[k];
         Sleep(TEMPO);
         iniciaCampo();
-
       }
-      else if (i == meioY - 1 && j<= meioX + 1 && j >= meioX - 1)
-      {
-
+      else if (i == meioY - 1 && j<= meioX + 1 && j >= meioX - 1) {
         k++;
         campo[i][j] = linha1[k];
         Sleep(TEMPO);
         iniciaCampo();
-
-      }
-      else if (i == meioY && j<= meioX + 6 && j >= meioX - 6)
-      {
-
+      } else if (i == meioY && j<= meioX + 6 && j >= meioX - 6) {
         k++;
         campo[i][j] = linha1[k];
         Sleep(TEMPO);
         iniciaCampo();
-
-      }
-      else if (i == meioY + 1 && j<= meioX + 2 && j >= meioX-1)
-      {
-
-        k++;
-        campo[i][j] = linha1[k];
-        Sleep(TEMPO);
-        iniciaCampo();
-
-      }
-      else if (i == meioY + 2 && j<= meioX + 6 && j >= meioX - 5)
-      {
-
-        k++;
-        campo[i][j] = linha1[k];
-        Sleep(TEMPO);
-        iniciaCampo();
-
       }
     }
   }
@@ -92,33 +62,15 @@ void logo()
 
 void apaga()
 {
-  int TEMPO2 = TEMPO - 45;
-  for(int i = MAXCIMA - 1; i >= meioY - 2 ; i--)
-  {
-    for(int j = MAXLADO - 1 ;  j >= meioX - 6; j--)
-    {
-      if(i == meioY - 2 && j >= meioX - 4 && j <= meioX + 5)
-      {
+  for(int i = MAXCIMA - 1; i >= meioY - 2 ; i--)  {
+    for(int j = MAXLADO - 1 ;  j >= meioX - 6; j--)    {
+      if(i == meioY - 2 && j >= meioX - 4 && j <= meioX + 5){
         campo[i][j] = ' ';
         iniciaCampo();
-      }
-      else if(i == meioY - 1 && j >= meioX && j <= meioX + 2)
-      {
+      }else if(i == meioY - 1 && j >= meioX && j <= meioX + 2){
         campo[i][j] = ' ';
         iniciaCampo();
-      }
-      else if(i == meioY && j >= meioX - 6 && j <= meioX + 7)
-      {
-        campo[i][j] = ' ';
-        iniciaCampo();
-      }
-      else if(i == meioY + 1 && j >= meioX && j <= meioX + 3)
-      {
-        campo[i][j] = ' ';
-        iniciaCampo();
-      }
-      else if(i == meioY + 2 && j >= meioX - 6 && j <= meioX + 6)
-      {
+      }else if(i == meioY && j >= meioX - 6 && j <= meioX + 7){
         campo[i][j] = ' ';
         iniciaCampo();
       }
@@ -126,21 +78,16 @@ void apaga()
   }
 }
 
-void IniciaMenu()
-{
+void IniciaMenu(){
   int k = -1;
   string frase1 = " - MENU - ";
   string start = " START ";
   string options = " OPTIONS ";
   string credits = " CREDITS ";
   int t = TEMPO - 25;
-  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++)
-  {
-    for(int j = meioX - 10;  j <=  MAXLADO - 2 ; j++)
-    {
-      if (i == meioY - 5 && j <= meioX + 4 && j >= meioX - 4)
-      {
-
+  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++){
+    for(int j = meioX - 10;  j <=  MAXLADO - 2 ; j++){
+      if (i == meioY - 5 && j <= meioX + 4 && j >= meioX - 4){
         k++;
         campo[i][j] = frase1[k];
         Sleep(t);
@@ -148,109 +95,78 @@ void IniciaMenu()
         if( j == meioX + 4) k = 0;
       }
 
-      if (i == meioY && j <= meioX + 2 && j >= meioX - 2)
-      {
-
+      if (i == meioY && j <= meioX + 2 && j >= meioX - 2){
         k++;
         campo[i][j] = start[k];
         Sleep(t);
         iniciaCampo();
         if( j == meioX + 2) k = 0;
-
       }
 
-      if (i == meioY + 2 && j <= meioX + 3 && j >= meioX - 3)
-      {
-
+      if (i == meioY + 2 && j <= meioX + 3 && j >= meioX - 3){
         k++;
         campo[i][j] = options[k];
         Sleep(t);
         iniciaCampo();
         if( j == meioX + 3) k = 0;
-
       }
 
-      if (i == meioY + 4 && j <= meioX + 3 && j >= meioX - 3)
-      {
-
+      if (i == meioY + 4 && j <= meioX + 3 && j >= meioX - 3){
         k++;
         campo[i][j] = credits[k];
         Sleep(t);
         iniciaCampo();
         if( j == meioX + 3) k = 0;
-
       }
     }
   }
 }
 
-void menu()
-{
+void menu(){
   int k = -1;
   string frase1 = " - MENU - ";
   string start = " START ";
   string options = " OPTIONS ";
   string credits = " CREDITS ";
-  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++)
-  {
-    for(int j = meioX - 10;  j <=  MAXLADO - 2 ; j++)
-    {
-      if (i == meioY - 5 && j <= meioX + 4 && j >= meioX - 4)
-      {
+  for(int i =  meioY - 10; i <= MAXCIMA - 1 ; i++){
+    for(int j = meioX - 10;  j <=  MAXLADO - 2 ; j++){
+      if (i == meioY - 5 && j <= meioX + 4 && j >= meioX - 4){
 
         k++;
         campo[i][j] = frase1[k];
         if( j == meioX + 4) k = 0;
-      }
-
-      else if (i == meioY && j <= meioX + 2 && j >= meioX - 2)
-      {
-
+      } else if (i == meioY && j <= meioX + 2 && j >= meioX - 2){
         k++;
         campo[i][j] = start[k];
         if( j == meioX + 2) k = 0;
-
-      }
-
-      else if (i == meioY + 2 && j <= meioX + 3 && j >= meioX - 3)
-      {
-
+      } else if (i == meioY + 2 && j <= meioX + 3 && j >= meioX - 3){
         k++;
         campo[i][j] = options[k];
         if( j == meioX + 3) k = 0;
-
-      }
-
-      else if (i == meioY + 4 && j <= meioX + 3 && j >= meioX - 3)
-      {
-
+      } else if (i == meioY + 4 && j <= meioX + 3 && j >= meioX - 3){
         k++;
         campo[i][j] = credits[k];
         if( j == meioX + 3) k = 0;
-
       }
     }
   }
   iniciaCampo();
 }
 
-void credits()
-{
+void credits(){
   logo();
   Sleep(500);
   apaga();
 }
 
-void escolha()
-{
+void escolha(){
   char k;
   int aux;
 
   aux = 1;
 
   menu();
-  do
-  {
+  do{
     switch (aux) {
       case 1:
       campo[meioY][meioX - 4] = '>';
@@ -259,20 +175,13 @@ void escolha()
       menu();
 
       k = getch();
-      if(k == 'w')
-      {
+      if(k == 'w'){
         aux--;
         if(aux == 0) aux = 2;
-
-      }
-      else if(k == 's')
-      {
+      } else if(k == 's'){
         aux ++;
         if(aux == 4) aux = 1;
-      }
-
-      else if(k == 'q')
-      {
+      } else if(k == 'q'){
         cout << "Really want to quit?\n Y - Yes N - no \n";
         k = getch();
         if(k == 'y') aux = 4;
@@ -285,19 +194,13 @@ void escolha()
         campo[meioY + 2][meioX - 5] = '>';
         menu();
         k = getch();
-        if(k == 'w')
-        {
+        if(k == 'w') {
           aux--;
           if(aux == 0) aux = 3;
-
-        }
-        else if(k == 's')
-        {
+        } else if(k == 's'){
           aux ++;
           if(aux == 4) aux = 1;
-        }
-        else if(k == 'q')
-        {
+        } else if(k == 'q'){
           cout << "Really want to quit?\n Y - Yes N - no \n";
           k = getch();
           if(k == 'y') aux = 4;
@@ -310,28 +213,19 @@ void escolha()
       campo[meioY + 2][meioX - 5] = ' ';
         menu();
         k = getch();
-        if(k == 'w')
-        {
+        if(k == 'w'){
           aux--;
           if(aux == 0) aux = 3;
 
-        }
-        else if(k == 's')
-        {
+        } else if(k == 's') {
           aux ++;
           if(aux == 4) aux = 1;
-        }
-        else if(k == 'q')
-        {
+        } else if(k == 'q'){
           cout << "Really want to quit?\n Y - Yes N - no \n";
           k = getch();
           if(k == 'y') aux = 4;
           else if (k == 'n') break;
-        }
-        else if(k == '\n') credits();
-
+        } else if(k == '\n') credits();
     }
-
-
   }while(k != '4');
 }
